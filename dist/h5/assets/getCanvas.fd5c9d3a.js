239b85610c1a3ea843f6cd6a76fc3bffefcdc282
tmui <369986563@qq.com>
2023-07-08 09:19:42 +0800

    新增h5编译后的demo

---
+++ dist/h5/assets/getCanvas.fd5c9d3a.js
@@ -0,0 +1,1 @@
+import{J as n,S as e}from"./index-5c06cf16.js";function o(o,t,i,s,r=!1){return new Promise(((i,s)=>{setTimeout((async function(){n().in(o),i({ctx:e(t,o),node:null})}),50)}))}export{o as g};
---
+++ dist/h5/assets/logo.c315c8a0.js
@@ -0,0 +1,1 @@
+const s="/h5/assets/logo-9300614a.png";export{s as l};
